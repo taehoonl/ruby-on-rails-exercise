fd450b4ec5d1e0ecf0af0b9c037107ec95780a37
Taehoon Lee <tl353@cornell.edu>
2013-02-18 21:57:02 +0000

    authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+	user = User.authenticate(params[:session][:email], params[:session][:password])
+	
+	if user
+		flash[:notice] = "Welcome, #{user.email}!" 
+		redirect_to user
+	else
+		flash[:error] = 'Invalid email/password combination'
+		redirect_to new_sessions_path	
+	end
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Info2310::Application.routes.draw do
   root :to => 'static_pages#home'
   resources :micro_posts
 
-  resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "static_pages/home"
 
